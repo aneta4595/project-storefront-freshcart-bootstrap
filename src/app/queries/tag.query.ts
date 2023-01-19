0155ee387bb4b31e5f7a5d9248227a2726a74a29
Aneta Walicka <aneta4595@wp.pl>
2023-01-19 21:32:13 +0100

    models and query model created

---
+++ src/app/queries/tag.query.ts
@@ -0,0 +1,7 @@
+export interface TagQuery {
+    readonly id: string;
+    readonly name: string;
+    readonly logoUrl: string;
+    readonly distanceInMeters: string;
+    readonly tagIds: string[];
+}
