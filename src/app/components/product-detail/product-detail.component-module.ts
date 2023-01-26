833a6d81b5a4c16c71681e165e896527b49761c8
Aneta Walicka <aneta4595@wp.pl>
2023-01-26 22:47:19 +0100

    product detail component created

---
+++ src/app/components/product-detail/product-detail.component-module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ProductDetailComponent } from './product-detail.component';
+
+@NgModule({
+  imports: [CommonModule, RouterModule],
+  declarations: [ProductDetailComponent],
+  providers: [],
+  exports: [ProductDetailComponent]
+})
+export class ProductDetailComponentModule {
+}
