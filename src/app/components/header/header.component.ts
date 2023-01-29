ceaede6341f6b3df0607262f49f7f538b3cfc756
Aneta Walicka <aneta4595@wp.pl>
2023-01-29 19:30:52 +0100

    mega menu added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,9 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { BehaviorSubject, Observable , take, tap} from 'rxjs';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+import { BehaviorSubject, Observable, of, take, tap } from 'rxjs';
 import { CategoryModel } from '../../models/category.model';
 import { CategoriesService } from '../../services/categories.service';
 
@@ -8,26 +12,65 @@ import { CategoriesService } from '../../services/categories.service';
   styleUrls: ['./header.component.scss'],
   templateUrl: './header.component.html',
   encapsulation: ViewEncapsulation.Emulated,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
-  readonly categoriesList$: Observable<CategoryModel[]> = this._categoriesService.getAllCategories();
+  readonly categoriesList$: Observable<CategoryModel[]> =
+    this._categoriesService.getAllCategories();
 
-  private _hamburgerMenuSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  public hamburgerMenu$: Observable<boolean> = this._hamburgerMenuSubject.asObservable();
+  private _hamburgerMenuSubject: BehaviorSubject<boolean> =
+    new BehaviorSubject<boolean>(false);
+  public hamburgerMenu$: Observable<boolean> =
+    this._hamburgerMenuSubject.asObservable();
 
-  constructor(private _categoriesService: CategoriesService) {
-  }
+  readonly dairyAndBread$: Observable<string[]> = of([
+    'Butter',
+    'Milk Drink',
+    'Curd & Yogurt',
+    'Eggs',
+    'Buns & Bakery',
+    'Cheese',
+    'Condensed Milk',
+    'Dairy Products',
+  ]);
+
+  readonly breakfast$: Observable<string[]> = of([
+    'Breakfast Cereal',
+    'Noodles, Pasta & Soup',
+    'Frozen Veg Snacks',
+    'Frozen Non-Veg Snacks',
+    'Vermicelli',
+    'Instant Mixes',
+    'Batter',
+    'Fruit and Juices',
+  ]);
+
+  readonly coldDrinks$: Observable<string[]> = of([
+    'Soft Drinks',
+    'Fruit Juices',
+    'Coldpress',
+    'Water & Ice Cubes',
+    'Soda & Mixers',
+    'Health Drinks',
+    'Herbal Drinks',
+    'Milk Drinks'
+  ]);
+
+  constructor(private _categoriesService: CategoriesService) {}
   showHamburgerMenu() {
-    this.hamburgerMenu$.pipe(
-      take(1),
-      tap(() => this._hamburgerMenuSubject.next(true))
-    ).subscribe()
+    this.hamburgerMenu$
+      .pipe(
+        take(1),
+        tap(() => this._hamburgerMenuSubject.next(true))
+      )
+      .subscribe();
   }
   hideHamburgerMenu() {
-    this.hamburgerMenu$.pipe(
-      take(1),
-      tap(() => this._hamburgerMenuSubject.next(false))
-    ).subscribe()
+    this.hamburgerMenu$
+      .pipe(
+        take(1),
+        tap(() => this._hamburgerMenuSubject.next(false))
+      )
+      .subscribe();
   }
 }
